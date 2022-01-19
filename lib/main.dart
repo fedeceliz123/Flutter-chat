840aa442d649fea3478838391886e6718cf0a6e7
unknown <fede_celiz92@hotmail.com>
2022-01-19 20:23:54 -0300

    fede

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:chat_tiempo_real/routes/routes.dart';
 
-void main()=>runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-    
-@override
-  Widget build (BuildContext context){
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: 'login',
-      
+      initialRoute: 'chat',
       routes: appRoute,
-     );
+    );
   }
 }
--- lib/pages/chat_pages.dart
+++ lib/pages/chat_pages.dart
@@ -1,14 +1,112 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
+class ChatPage extends StatefulWidget {
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
 
-class ChatPage extends StatelessWidget {
-
+class _ChatPageState extends State<ChatPage> {
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Chat'),
-     ),
-   );
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          centerTitle: true,
+          title: Column(
+            children: [
+              CircleAvatar(
+                child: Text(
+                  'Te',
+                  style: TextStyle(fontSize: 12),
+                ),
+                backgroundColor: Colors.blueAccent,
+                maxRadius: 14,
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                'Tereza',
+                style: TextStyle(color: Colors.black87, fontSize: 12),
+              )
+            ],
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  // lista de chats
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (_, i) => Text('$i'),
+                  reverse: true,
+                ),
+              ),
+              Divider(
+                height: 1,
+              ),
+              Container(
+                color: Colors.white,
+                child: _inputChat(),
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+              child: TextField(
+            controller: _textController,
+            onSubmitted: _handleSubmit,
+            onChanged: (String texto) {
+              // cuando esta escribiendo para q avise
+            },
+            decoration: InputDecoration(hintText: 'Enviar mensaje'),
+            focusNode: _focusNode,
+          )),
+          //boton de enviar
+          Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS
+                  ?
+                  //boton ios
+                  CupertinoButton(
+                      child: Text('Enviar'),
+                      onPressed: () {},
+                    )
+                  //boton andriod
+                  : Container(
+                      margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.send,
+                          color: Colors.blue.shade400,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ))
+        ],
+      ),
+    ));
+  }
+
+  _handleSubmit(String texto) {
+    print(texto);
+    //limpiar caja de texto
+    _textController.clear();
+    // dejar el teclado levantado
+    _focusNode.requestFocus();
   }
 }
